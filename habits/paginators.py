8fa7a709a5fa0ca8920fece906f48f223a1a0033
RVBelyaevsky <rooooooma73@gmail.com>
2025-02-23 18:46:04 +0800

    paginator

--- habits/paginators.py
+++ habits/paginators.py
@@ -4,4 +4,4 @@ from rest_framework import pagination
 class HabitPaginator(pagination.PageNumberPagination):
     page_size = 5
     page_query_param = 'page_size'
-    max_page_size = 25
+    max_page_size = 20
