f172564cc89ddf612d96cd7ac879d0d6c3cc3431
RVBelyaevsky <rooooooma73@gmail.com>
2025-02-23 13:14:11 +0800

    validators

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
 from habits.models import Habit
+from habits.validators import RewardOrLinkedValidator, LimitTimeСompleteValidator, LinkedOnlyEnjoyableValidator, \
+    EnjoyableNotRewardNotLinked, PeriodicityValidator
 
 
 class HabitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habit
         fields = "__all__"
+        validators = [
+            RewardOrLinkedValidator(field1='reward', field2='linked_habit'),
+            LimitTimeСompleteValidator(),
+            LinkedOnlyEnjoyableValidator(),
+            EnjoyableNotRewardNotLinked(),
+            PeriodicityValidator()
+        ]
