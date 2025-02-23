805e0ac53caf6fdc6a95a74463ee09150c462f63
RVBelyaevsky <rooooooma73@gmail.com>
2025-02-23 18:45:07 +0800

    validators

--- habits/views.py
+++ habits/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from rest_framework import viewsets
 
 from habits.models import Habit
+from habits.paginators import HabitPaginator
 from habits.serializers import HabitSerializer
 
 
 class HabitViewSet(viewsets.ModelViewSet):
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
+    pagination_class = HabitPaginator
